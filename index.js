6a51bdd21a06e1e9f0501abc4bcabbd9ad823119
Visavilwa Daisy <135525440+visavilwa1@users.noreply.github.com>
2023-12-04 17:03:48 +0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 //document.getElementById("count").innerText = 5
 
-let count = 5 
-count = count + 10
- console.log(count)
- //work
- let bonusPoints = 50
- console.log(bonusPoints)
+function increment(){
+    console.log("The button was clicked")
+}
